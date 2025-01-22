1db54a3619684a2de902a83db0608d73609cac80
fishdan <fishdan@yahoo.com>
2025-01-22 11:10:23 -0500

    Initial push

---
+++ src/main/java/com/fishdan/myorgchart/MyOrgChartApplication.java
@@ -0,0 +1,15 @@
+package com.fishdan.myorgchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrgChartApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting");
+        SpringApplication.run(MyOrgChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fishdan/myorgchart/MyOrgChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishdan.myorgchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOrgChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
